02f2286bfb61c0a124801052db178d8f843016a4
Claire <clairesay13@gmail.com>
2021-04-23 12:20:15 +1000

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+// import express
+const express = require('express');
+
+// initialise express
+const app = express();
+
+// serve static files from the public folder
+app.use(express.static('public'));
+
+// serve the index file for the root / path - so if there 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+// start the server, listen for incoming traffic and print message if successful
+let server = app.listen(8888, function(){
+    console.log("App server is running on port 8888");
+});
